54b9215b7d746b32dab42b126be20dc1c25129dd
BeSplinter <abzacc@bk.ru>
2024-04-18 12:25:07 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="contaner">
+      <div className="col-3">
+        <div className="nav flex-column nav-pills">
+          <a href="" className="nav-link">
+            Главная
+          </a>
+          <a href="" className="nav-link">
+            Профиль
+          </a>
+          <a href="" className="nav-link">
+            Друзья
+          </a>
+          <a href="" className="nav-link">
+            Магазин
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
